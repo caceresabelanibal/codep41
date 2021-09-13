f94e9565a4905a364a2ecf158bcb716d330a6528
caceresabelanibal <72995924+caceresabelanibal@users.noreply.github.com>
2021-09-13 16:05:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-
+<?php
+$ts = date('Y-d-m');
+$ip = $_SERVER['REMOTE_ADDR'];
+echo "{";
+echo "<br>";
+echo '"timestamp": ', json_encode($ts, JSON_FORCE_OBJECT), "\n";
+echo "<br>";
+echo '"ip": ', json_encode($ip, JSON_FORCE_OBJECT), "\n";
+echo "<br>";
+echo "}";
+?>
